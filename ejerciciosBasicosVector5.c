f9199b567120aaa9dcc2b0bbc80edc6e360f4689
Carlos Serrano <carlosserrano@MacBook-Pro-de-Carlos.local>
2022-02-03 19:01:30 +0100

    Cuenta tacos

--- ejerciciosBasicosVector5.c
+++ ejerciciosBasicosVector5.c
@@ -4,7 +4,8 @@ int hayPalabrotas(char m[256]);
 char* filtraPalabrotas(char m[256]);
 
 int main(){
-    char mensaje[256]=" Papá, quiero caca y pipi ";
+    char mensaje[256]=" Papá, quiero caca y pipi, he dicho caca ";
+    printf(" \n Hay %d tacos \n",hayPalabrotas(mensaje));
 }
 
 int hayPalabrotas(char m[256]){
@@ -12,6 +13,37 @@ int hayPalabrotas(char m[256]){
         "caca",
         "pipi"
     };
+    int nTacos=0;
+
+    int i=0;
+    while(i<256&&m[i]!='\0'){
+        //Buscando en el array de palabrotas la primera letra
+        for(int j=0;j<2;j++){
+            if(m[i]==tacos[j][0] && (i==0 || m[i-1]==' ')){
+                int tmp=i; //guardo la posición actual de la cadena por si hay que rebobinar
+                int k=0;
+                for(k=1;k<25&&tacos[j][k]!='\0';k++){
+                    if(m[++i]!=tacos[j][k]){
+                        k=25;  //si no coincide en alguna letra rompo el bucle
+                    }
+                }
+                if(tacos[j][k]=='\0' && (m[i+1]==' ' || m[i+1]==',' || 
+                                        m[i+1]=='.' || m[i+1]=='\0')){
+                    //si me he salido del bucle llegando al final de un
+                    //taco y la siguiente letra del mensaje es espacio o final
+                    // es un taco
+                    nTacos++;
+                }else{
+                    //si no es que todo ha fallado, rebobino para seguir por donde iba
+                    i=tmp;
+                }
+            }
+        }
+        i++;
+    
+    
+    }
+    return nTacos;
 
 }
 
@@ -20,4 +52,5 @@ char* filtraPalabrotas(char m[256]){
         "caca",
         "pipi"
     };
+    return NULL;
 }
